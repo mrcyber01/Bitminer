a4ef26470bd5321680e67370c421848cc5cf60f7
mrcyber01 <tropadosete7777@gmail.com>
2025-04-14 13:26:49 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,25 +1,25 @@
 const logins = {
   "user1@email.com": {
     password: "F4G7H2J9",
-    redirect: "https://mrcyber01.github.io/Bitminer/F4G7H2J9.html"
+    redirect: "F4G7H2J9.html"  // Caminho para o diretório local
   },
   "admin@miner.com": {
     password: "B3K1D8P0",
-    redirect: "B3K1D8P0.html"
+    redirect: "diretorio/B3K1D8P0.html"  // Caminho para o diretório local
   },
   "teste@btc.com": {
     password: "W9X2V5L1",
-    redirect: "W9X2V5L1.html"
+    redirect: "diretorio/W9X2V5L1.html"  // Caminho para o diretório local
   }
 };
 
 function login() {
-  const email = document.getElementById("email").value.trim().toLowerCase();  // Converte o email para minúsculas
+  const email = document.getElementById("email").value.trim().toLowerCase();
   const password = document.getElementById("password").value.trim();
   const error = document.getElementById("error");
 
   if (logins[email] && logins[email].password === password) {
-    window.location.href = logins[email].redirect;
+    window.location.href = logins[email].redirect;  // Redireciona para o diretório local
   } else {
     error.textContent = "E-mail ou senha incorretos.";
   }
