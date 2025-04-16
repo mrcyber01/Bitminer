f922c2c3636fdc02b22cce90a46bfbee95594b00
mrcyber01 <tropadosete7777@gmail.com>
2025-04-16 16:07:03 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,15 +1,15 @@
 const logins = {
-  "Oliveiralaira271@gmail.com": {
+  "oliveiralaira271@gmail.com": {
     password: "F4G7H2J9",
-    redirect: "F4G7H2J9.html"  // Caminho para o diretório local
+    redirect: "F4G7H2J9.html"
   },
   "admin@miner.com": {
     password: "B3K1D8P0",
-    redirect: "diretorio/B3K1D8P0.html"  // Caminho para o diretório local
+    redirect: "diretorio/B3K1D8P0.html"
   },
   "teste@btc.com": {
     password: "W9X2V5L1",
-    redirect: "diretorio/W9X2V5L1.html"  // Caminho para o diretório local
+    redirect: "diretorio/W9X2V5L1.html"
   }
 };
 
@@ -19,7 +19,7 @@ function login() {
   const error = document.getElementById("error");
 
   if (logins[email] && logins[email].password === password) {
-    window.location.href = logins[email].redirect;  // Redireciona para o diretório local
+    window.location.href = logins[email].redirect;
   } else {
     error.textContent = "E-mail ou senha incorretos.";
   }
