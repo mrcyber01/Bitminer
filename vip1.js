565db63d730c6bca25a487294b44694f4c523aa1
mrcyber01 <tropadosete7777@gmail.com>
2025-04-18 14:05:04 -0300

    Update vip1.js

--- vip1.js
+++ vip1.js
@@ -19,8 +19,8 @@ let statsInterval;
 let lastOverheatWarning = 0;
 
 // CONFIGURAÇÃO MANUAL:
-const miningDuration = 600; // segundos até concluir a mineração
-const rewardBTC = 0.000000021; // recompensa total a ser minerada
+const miningDuration = 1200; // segundos até concluir a mineração
+const rewardBTC = 0.00000021; // recompensa total a ser minerada
 
 startBtn.addEventListener("click", () => {
   if (mining || miningFinished) return; // impede novas minerações
